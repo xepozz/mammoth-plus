85fe4299de218a6beac0c90202e3e250058ea02d
Dmitriy Derepko <xepozz@list.ru>
2025-07-21 11:48:28 +0300

    fix tests

--- test/main.tests.js
+++ test/main.tests.js
@@ -38,26 +38,24 @@ describe("main tests", function() {
     });
 
     it("images are written to separate files if output dir is set", function() {
-        return createTempDir().then(function(tempDir) {
-            var outputPath = path.join(tempDir, "tiny-picture.html");
-            var imagePath = path.join(tempDir, "1.png");
-            return runmammoth(testPath("tiny-picture.docx"), "--output-dir", tempDir).then(function(result) {
-                assert.equal(result.stderrOutput, "");
-                assert.equal(result.output, "");
-                assert.equal(fs.readFileSync(outputPath, "utf8"), '<p><img src="1.png" /></p>');
-                assert.equal(fs.readFileSync(imagePath, "base64"), imageBase64);
-            });
+        const tempDir = temp.mkdirSync();
+        var outputPath = path.join(tempDir, "tiny-picture.html");
+        var imagePath = path.join(tempDir, "1.png");
+        return runmammoth(testPath("tiny-picture.docx"), "--output-dir", tempDir).then(function(result) {
+            assert.equal(result.stderrOutput, "");
+            assert.equal(result.output, "");
+            assert.equal(fs.readFileSync(outputPath, "utf8"), '<p><img src="1.png" /></p>');
+            assert.equal(fs.readFileSync(imagePath, "base64"), imageBase64);
         });
     });
 
     it("style map is used if set", function() {
-        return createTempDir().then(function(tempDir) {
-            var styleMapPath = path.join(tempDir, "style-map");
-            fs.writeFileSync(styleMapPath, "p => span:fresh");
-            return runmammoth(testPath("single-paragraph.docx"), "--style-map", styleMapPath).then(function(result) {
-                assert.equal(result.stderrOutput, "");
-                assert.equal(result.output, "<span>Walking on imported air</span>");
-            });
+        const tempDir = temp.mkdirSync();
+        var styleMapPath = path.join(tempDir, "style-map");
+        fs.writeFileSync(styleMapPath, "p => span:fresh");
+        return runmammoth(testPath("single-paragraph.docx"), "--style-map", styleMapPath).then(function(result) {
+            assert.equal(result.stderrOutput, "");
+            assert.equal(result.output, "<span>Walking on imported air</span>");
         });
     });
 
@@ -67,26 +65,20 @@ describe("main tests", function() {
             assert.equal(result.output, "Walking on imported air\n\n");
         });
     });
-
-
-    function runmammoth() {
-        var args = Array.prototype.slice.call(arguments, 0);
-        var deferred = promises.defer();
-
-        var processArgs = ["node", "bin/mammoth"].concat(args);
-        // TODO: proper escaping of args
-        var command = processArgs.join(" ");
-        child_process.exec(command, function(error, stdout, stderr) { // eslint-disable-line camelcase
-            console.log(stderr);
-            assert.equal(error, null);
-            deferred.resolve({output: stdout, stderrOutput: stderr});
-        });
-
-        return deferred.promise;
-    }
-
-    function createTempDir() {
-        return promises.nfcall(temp.mkdirSync, null);
-    }
-
 });
+
+function runmammoth() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var deferred = promises.defer();
+
+    var processArgs = ["node", "bin/mammoth"].concat(args);
+    // TODO: proper escaping of args
+    var command = processArgs.join(" ");
+    child_process.exec(command, function(error, stdout, stderr) { // eslint-disable-line camelcase
+        console.log(stderr);
+        assert.equal(error, null);
+        deferred.resolve({output: stdout, stderrOutput: stderr});
+    });
+
+    return deferred.promise;
+}
